55bb0844969e98ba26fbaebb143ee37df145803e
unknown <mir535214@gmail.com>
2024-07-24 14:01:30 +0530

    first-commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,42 +9,46 @@ const JWT_SECRET = "test123";
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: "http://localhost:5173"
-}));
+    origin: "http://localhost:5173",
+  })
+);
 
 app.post("/signin", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    // do db validations, fetch id of user from db
-    const token = jwt.sign({
-        id: 1
-    }, JWT_SECRET);
-    res.cookie("token", token);
-    res.send("Logged in!");
+  const email = req.body.email;
+  const password = req.body.password;
+  // do db validations, fetch id of user from db
+  const token = jwt.sign(
+    {
+      id: 1,
+    },
+    JWT_SECRET
+  );
+  res.cookie("token", token);
+  //will put the cookie in the set cookie header
+  res.send("Logged in!");
 });
 
 app.get("/user", (req, res) => {
-    const token = req.cookies.token;
-    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
-    // Get email of the user from the database
-    res.send({
-        userId: decoded.id
-    })
+  const token = req.cookies.token;
+  const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+  // Get email of the user from the database
+  res.send({
+    userId: decoded.id,
+  });
 });
-
 
 app.post("/logout", (req, res) => {
-    res.cookie("token", "ads");
-    res.json({
-        message: "Logged out!"
-    })
+  res.cookie("token", "ads");
+  res.json({
+    message: "Logged out!",
+  });
 });
 
-
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../src/index.html"))
-})
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
 
 app.listen(3000);
